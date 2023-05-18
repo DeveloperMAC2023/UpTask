989156dbf81ddac4314e0d3a1adac0acd20aa198
DeveloperMAC2023 <DeveloperMAC2023@gmail.com>
2023-05-18 15:47:52 -0400

    Finaizada rama VistaLogin

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -2,23 +2,31 @@
 
 namespace Controllers;
 
+use MVC\Router;
+
 class LoginController{
-    public static function login(){        
-        echo 'desde login';
+
+    public static function login(Router $router){         
         if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 
         }
+        $router->render('auth/login', [
+            'titulo' => 'Iniciar Sesión'
+        ]);
     }
 
     public static function logout(){
         echo 'desde logout';
     }
 
-    public static function crear(){        
-        echo 'desde crear';
+    public static function crear(Router $router){        
         if ($_SERVER['REQUEST_METHOD'] === 'POST'){
             
         }
+        $router->render('auth/crear', [
+            'titulo' => 'Crea tu cuenta en UpTask'
+        ]);
+
     }
 
     public static function olvide(){        
