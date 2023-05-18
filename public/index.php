7e0fedc6741e0f3888dcf88a1763eaebdf4274fc
DeveloperMAC2023 <DeveloperMAC2023@gmail.com>
2023-05-18 12:17:39 -0400

    Terminada rama Primeros Pasos

--- public/index.php
+++ public/index.php
@@ -3,10 +3,29 @@
 require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
+use Controllers\LoginController;
 $router = new Router();
 
+// Login
+$router->get('/', [LoginController::class, 'login']);
+$router->post('/', [LoginController::class, 'login']);
+$router->get('/', [LoginController::class, 'logout']);
 
+// Crear Cuenta
+$router->get('/crear', [LoginController::class, 'crear']);
+$router->post('/crear', [LoginController::class, 'crear']);
 
+// Formulario de olvide mi password
+$router->get('/olvide', [LoginController::class, 'olvide']);
+$router->post('/olvide', [LoginController::class, 'olvide']);
+
+// Colocar el nuevo password
+$router->get('/reestablecer', [LoginController::class, 'reestablecer']);
+$router->post('/reestablecer', [LoginController::class, 'reestablecer']);
+
+// Confirmación de Cuenta
+$router->get('/mensaje', [LoginController::class, 'mensaje']);
+$router->get('/confirmar', [LoginController::class, 'confirmar']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
