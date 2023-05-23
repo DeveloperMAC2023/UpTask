fb6ac3f13832931123d20398d2a81ceca13aa880
DeveloperMAC2023 <DeveloperMAC2023@gmail.com>
2023-05-22 21:47:59 -0400

    Finalizada la rama CrandoLogin

---
+++ classes/Email.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Classes;
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+Class Email {
+
+    public $email;
+    public $nombre;
+    public $token;
+
+    public function __construct($email, $nombre, $token) {
+        $this->email = $email;
+        $this->nombre = $nombre;
+        $this->token = $token;
+    }
+
+    public function enviarConfirmacion() {
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->Host = 'smtp.mailtrap.io';
+        $mail->SMTPAuth = true;
+        $mail->Port = 2525;
+        $mail->Username ='3ae3ae3b1bf88f';
+        $mail->Password = '0ce1f5b5a668a1';
+         
+        $mail->setFROM('cuentas@uptask.com');
+        $mail->addAddress('cuentas@uptask.com','UpTask.com');
+        $mail->Subject = 'Confirma tu cuenta';
+        
+        // Set HTML
+        $mail->isHTML(TRUE);
+        $mail->CharSet = 'UTF-8';
+
+        $contenido = "<html>";
+        $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> Has creado tu cuenta en App Salon, solo debes confirmarla presionando el siguiente enlace</p>";
+        $contenido .= "<p>Presiona aquí: <a href='http://localhost:3000/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a> </p>";
+        $contenido .= "<p>Si tu no solicitaste una cuenta, puedes ignorar este mensaje</p>";
+        $contenido .= "</html>";
+
+        $mail->Body = $contenido;
+                
+        // Enviar mail
+        $mail->send();
+
+    }
+
+    public function enviarInstrucciones() {
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->Host = 'smtp.mailtrap.io';
+        $mail->SMTPAuth = true;
+        $mail->Port = 2525;
+        $mail->Username ='3ae3ae3b1bf88f';
+        $mail->Password = '0ce1f5b5a668a1';
+         
+        $mail->setFROM('cuentas@appsalon.com');
+        $mail->addAddress('cuentas@appsalon.com','AppSalon.com');
+        $mail->Subject = 'Reestablece tu Password';
+        
+        // Set HTML
+        $mail->isHTML(TRUE);
+        $mail->CharSet = 'UTF-8';
+
+        $contenido = "<html>";
+        $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> Has solicitado reestablecer tu password, sigue el siguiente enlace para hacerlo.</p>";
+        $contenido .= "<p>Presiona aquí: <a href='http://localhost:3000/recuperar?token=" . $this->token . "'>Reestablecer Password</a> </p>";
+        $contenido .= "<p>Si tu no solicitaste una cuenta, puedes ignorar este mensaje</p>";
+        $contenido .= "</html>";
+
+        $mail->Body = $contenido;
+                
+        // Enviar mail
+        $mail->send();
+
+    }
+
+
+}
--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -3,6 +3,8 @@
 namespace Controllers;
 
 use MVC\Router;
+use Classes\Email;
+use Model\Usuario;
 
 class LoginController{
 
@@ -20,21 +22,50 @@ class LoginController{
     }
 
     public static function crear(Router $router){        
+        $usuario = new Usuario;
+        $alertas = [];
         if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-            
+            $usuario->sincronizar($_POST);
+            $alertas = $usuario->validarNuevaCuenta();
+            if (empty($alertas)) {
+                $existeUsuario = Usuario::where('email','$usuario->email');
+                if ($existeUsuario) {
+                    Usuario::setAlerta('error','El usuario ya esta registrado');
+                    $alertas = Usuario::getAlertas();
+                } else {
+                    // Hashear password
+                    $usuario->hashPassword();
+                    // Eliminar password 2
+                    unset($usuario->password2);
+                    // Generar Token
+                    $usuario->crearToken();
+                    // Crear usuario
+                    $resultado = $usuario->guardar();
+                    // Enviar Email
+                    $email = new Email($usuario->email, $usuario->nombre, $usuario->token);
+                    if ($resultado) {
+                        header('Location: /mensaje');
+                    }
+                }    
+            }
         }
         $router->render('auth/crear', [
-            'titulo' => 'Crea tu cuenta en UpTask'
+            'titulo' => 'Crea tu cuenta en UpTask',
+            'usuario' => $usuario,
+            'alertas' => $alertas
         ]);
 
     }
 
     public static function olvide(Router $router){        
+        $alertas = [];
         if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-            
+            $usuario = new Usuario($_POST);
+            $alertas = $usuario->validarEmail();
         }
         $router->render('auth/olvide', [
-            'titulo' => 'Olvide mi Password'
+            'titulo' => 'Olvide mi Password',
+            'alertas' => $alertas
         ]);
     }
 
@@ -54,8 +85,24 @@ class LoginController{
     }
 
     public static function confirmar(Router $router){
+        $token = s($_GET['token']);
+        if (!token) {
+            header('Location: /');
+        }
+        $usuario = Usuario::where('token',$token);
+        if (empty($usuario)) {
+            Usuario::setAlerta('error','Token no válido');
+        } else {
+            $usuario->confirmado =1;
+            $usuario->token = null;
+            unset($usuario->password2);
+            $usuario->guardar();
+            Usuario::setAlerta('exito','Cuenta Comprada Correctamente');
+        }
+        $alertas = Usuario::getAlertas();
         $router->render('auth/confirmar', [
-            'titulo' => 'Confirma tu Cuenta UpTask'
+            'titulo' => 'Confirma tu Cuenta UpTask',
+            'alertas' => $alertas
         ]);
     }
 
