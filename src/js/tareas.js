4a5f748897658c079608a8833773336dc244be58
DeveloperMAC2023 <DeveloperMAC2023@gmail.com>
2023-05-25 17:08:11 -0400

    Finalizada rama Formulario

---
+++ src/js/tareas.js
@@ -0,0 +1,41 @@
+(function() {
+    // Botón para mostrar el modal de agregar tarea
+    const nuevaTareaBtn = document.querySelector('#agregar-tarea');
+    nuevaTareaBtn.addEventListener('click', mostrarFormulario);
+
+    function mostrarFormulario() {
+        const modal = document.createElement('DIV');
+        modal.classList.add('modal');
+        modal.innerHTML = `
+            <form class="formulario nueva-tarea">
+                <legend>Añade una nueva tarea</legend>
+                <div class="campo">
+                    <label>Tarea</label>
+                    <input type="text" name="tarea" placeholder="Añadir tarea al proyecto actual" id="tarea"></input>
+                </div>
+                <div class="opciones">
+                    <input type="submit" class="submit-nueva-tarea" value="Añadir Tarea"></input>
+                    <button type="button" class="cerrar-modal">Cancelar</button>
+                </div>
+            </form>
+        `;
+        setTimeout(() => {
+            const formulario = document.querySelector('.formulario');
+            formulario.classList.add('animar');
+        }, 0);
+
+        modal.addEventListener('click', function(e) {
+            e.preventDefault();
+            if (e.target.classList.contains('cerrar-modal')) {
+                const formulario = document.querySelector('.formulario');
+                formulario.classList.add('cerrar');
+                setTimeout( () => {
+                    modal.remove();
+                },1000);
+                
+            }
+        })
+        document.querySelector('body').appendChild(modal);
+    }
+
+})();
